60efcc18920890ac22c13f67f7ccc24d373542a3
Michael Aquilina <michaelaquilina@gmail.com>
2021-12-17 23:45:20 +0000

    Add day07

---
+++ day07/src/main.rs
@@ -0,0 +1,65 @@
+use cached::proc_macro::cached;
+use itertools::sorted;
+use std::error::Error;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buffer = String::new();
+
+    std::io::stdin().read_to_string(&mut buffer)?;
+
+    let buffer = buffer.strip_suffix('\n').ok_or("invalid input")?;
+
+    let mut crabs = vec![];
+    for value in buffer.split(',') {
+        crabs.push(value.parse::<i32>()?);
+    }
+    println!("Part 1: {}", calculate_part_1(&crabs));
+    println!("Part 2: {}", calculate_part_2(&crabs)?);
+
+    Ok(())
+}
+
+fn calculate_part_1(crabs: &[i32]) -> i32 {
+    let med = median(crabs);
+    crabs.iter().map(|x| (med - x).abs()).sum()
+}
+
+fn calculate_part_2(crabs: &[i32]) -> Result<u32, Box<dyn Error>> {
+    let max = *crabs.iter().max().ok_or("empty")?;
+    let min = *crabs.iter().min().ok_or("empty")?;
+    let mut result = u32::MAX;
+
+    for value in min..max {
+        let cost = calculate_total_real_fuel_cost(crabs, value);
+        if cost < result {
+            result = cost;
+        }
+    }
+
+    Ok(result)
+}
+
+fn calculate_total_real_fuel_cost(crabs: &[i32], value: i32) -> u32 {
+    let mut cost = 0;
+    for crab in crabs {
+        cost += real_fuel_cost((crab - value).abs() as u32);
+    }
+    cost
+}
+
+#[cached]
+fn real_fuel_cost(distance: u32) -> u32 {
+    let mut cost = 0;
+    let mut index = 1;
+    while index <= distance {
+        cost += index;
+        index += 1;
+    }
+    cost
+}
+
+fn median(values: &[i32]) -> i32 {
+    let values = sorted(values).collect::<Vec<&i32>>();
+    *values[values.len() / 2]
+}
